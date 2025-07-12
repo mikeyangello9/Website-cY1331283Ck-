100520f03f85548e3b35aec7fa108177c3051fd8
michael aiwekhoe <aiwekhoemicheal@gmail.com>
2025-07-12 09:36:48 +0100

    terminal fixed?

--- public/iframe-assets/terminal.js
+++ public/iframe-assets/terminal.js
@@ -1,6 +1,39 @@
 // import { marked } from "marked";
 // import NotesData from "../../src/NotesData.js";
 
+const NotesData = [
+ 
+    {
+        id: 1,
+        folder: "Exploitation",
+        title: "Heap and Heap Overflow",
+        body: "Exploitation/Heap_Overflow_Cleaned.md",
+        topic: ["C", "Heap", "Exploitation"],
+        date: "28-06-2025"
+    },
+
+    {
+        id: 2,
+        title: "Introduction",
+        body: "Blog001.md",
+        topic: ["About", "intro", "Recon"],
+        date: "30-06-2025"
+    },
+    
+    {
+        id: 3,
+        title: "SQL basics for SQLi",
+        folder: "Portswigger",
+        body: "Portswigger/SQL injection.md",
+        topic: ["web", "burpsuite", "Portswigger"],
+        date: "28-06-2025"
+    },
+
+    
+   
+]
+//band aid fix
+
 const terminal = () => {
     
     const asciiFetch = document.querySelector(".fetch-ascii")
